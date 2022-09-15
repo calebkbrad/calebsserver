4a29bd5b4b8e9019990a389be88def818176c828
calebkbrad <92956463+calebkbrad@users.noreply.github.com>
2022-09-15 12:27:08 -0400

    added shebang

--- echo_server.py
+++ echo_server.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+
 import socket
+
 HOST = "0.0.0.0"
 PORT = 8080
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 s.listen()
 print(f"Listening on {HOST}:{PORT} for HTTP connections")
+
 while True:
     conn, addr = s.accept()
     print(f"Connected to {addr}")
