6742c212d59648160fa9f9069e7111b626bd3552
calebkbrad <cbrad022@odu.edu>
2022-09-19 20:34:44 -0400

    fixed longer messages for real

--- echoserver/echo_server.py
+++ echoserver/echo_server.py
@@ -10,15 +10,15 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 s.listen()
 print(f"Listening on {HOST}:{PORT} for HTTP connections")
-
+data = B""
 while True:
-    frags = []
     conn, addr = s.accept()
     print(f"Connected to {addr}")
-    data_frag = conn.recv(1024)
-    frags.append(data_frag)
-    if not data_frag:
-        data = "".join(frags)
-        conn.sendall(data)
-        conn.close()
-        break
+    while True:
+        data_frag = conn.recv(1024)
+        data += data_frag
+        if len(data_frag) < 1024:   
+            conn.sendall(data)
+            conn.close()
+            break
+    break
