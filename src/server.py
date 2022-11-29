5bcbdd89ac4a34a7d3ec19a6e83d023dc763768c
calebkbrad <cbrad022@odu.edu>
2022-11-28 22:43:31 -0500

    fix allow on delete

--- src/server.py
+++ src/server.py
@@ -197,7 +197,7 @@ def validate_and_get_request_info(http_request: bytes) -> tuple:
     payload_length = -1
     print(exists(uri))
     print(check_if_auth(uri))
-    if (exists(uri) and check_if_auth(uri)) or method == "PUT":
+    if (exists(uri) and check_if_auth(uri)) or method == "PUT" or method == "DELETE":
         path_to_auth = check_if_auth(uri)
         auth_type, realm, users, allow = parse_auth_file(path_to_auth)
     else:
